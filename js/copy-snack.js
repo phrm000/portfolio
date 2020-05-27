734db251e1b5a1b20791654474e4e3a9ff169bdf
Pedro M <phrm000@gmail.com>
2020-05-27 09:33:46 -0300

    criação repositorio

---
+++ js/copy-snack.js
@@ -0,0 +1,33 @@
+const celText = document.querySelector('#celSnack');
+const celBtnlogin = document.querySelector('#celCopy');
+const celBtn = document.querySelector('.celCopy');
+
+const emailText = document.querySelector('#emailSnack');
+const emailBtnlogin = document.querySelector('#emailCopy');
+const emailBtn = document.querySelector('.emailCopy');
+
+
+celBtn.addEventListener("click",snackShow);
+emailBtn.addEventListener("click",snackShow);
+
+celBtnlogin.addEventListener("click",snackShow);
+emailBtnlogin.addEventListener("click",snackShow);
+
+function snackShow(event){
+    selecionadoCopy = event.target;
+
+    if (selecionadoCopy.classList == 'celCopy' || selecionadoCopy.id == 'celCopy') {
+        var selecionadoCopyText = celText;
+    }
+
+    else if (selecionadoCopy.classList == 'emailCopy' || selecionadoCopy.id == 'emailCopy') {
+       var selecionadoCopyText = emailText;
+       
+    }  
+       
+        selecionadoCopyText.classList.replace('snackbar-hidden','snackbar-show');
+    setTimeout(function()
+    {
+        selecionadoCopyText.classList.replace('snackbar-show','snackbar-hidden'); 
+    }, 3000);
+}
---
+++ js/loader.js
@@ -0,0 +1,12 @@
+var loadedpage = function(){
+    $(".se-pre-con").fadeOut("slow");
+  };
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).on("load", function() {
+        // Animate loader off screen
+
+        $(".se-pre-con img").addClass("loaded-img");
+        
+        setTimeout(loadedpage, 2000);
+	});
