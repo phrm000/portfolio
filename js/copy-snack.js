531387164feb02ca6c9b77007163d466095cccbb
Pedro M <phrm000@gmail.com>
2020-05-29 09:50:11 -0300

    função de cópia implementada

--- js/copy-snack.js
+++ js/copy-snack.js
@@ -1,10 +1,13 @@
 const celText = document.querySelector('#celSnack');
 const celBtnlogin = document.querySelector('#celCopy');
 const celBtn = document.querySelector('.celCopy');
+const celNumber = document.querySelector('#number');
 
 const emailText = document.querySelector('#emailSnack');
 const emailBtnlogin = document.querySelector('#emailCopy');
 const emailBtn = document.querySelector('.emailCopy');
+const emailAdress = document.querySelector('#number');
+
 
 
 celBtn.addEventListener("click",snackShow);
@@ -17,10 +20,12 @@ function snackShow(event){
     selecionadoCopy = event.target;
 
     if (selecionadoCopy.classList == 'celCopy' || selecionadoCopy.id == 'celCopy') {
+        copyToClipboard('celNumber');
         var selecionadoCopyText = celText;
     }
 
     else if (selecionadoCopy.classList == 'emailCopy' || selecionadoCopy.id == 'emailCopy') {
+        copyToClipboard('emailAdress');
        var selecionadoCopyText = emailText;
        
     }  
@@ -31,3 +36,13 @@ function snackShow(event){
         selecionadoCopyText.classList.replace('snackbar-show','snackbar-hidden'); 
     }, 2750);
 }
+
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  
