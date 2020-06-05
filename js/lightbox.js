0a19d53d5cd12dda04ee15e16630a5649dd054e2
Pedro M <phrm000@gmail.com>
2020-06-05 10:17:33 -0300

    Implementação de lightbox em imagens

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,19 +1,33 @@
 const imgList = document.querySelectorAll(".works-display li");
+const worksList = document.querySelectorAll(".works-display");
 const Xclick = document.querySelector(".closeX");
+const lightBoxThumb = document.querySelector(".modal-content_thumb");
+const lightBoxPrincipal = document.querySelector(".modal-content_principal-image");
+
 console.log(imgList);
+Xclick.addEventListener("click", CloselightBox);
 
 for (let i = 0; i < imgList.length; i++) {
     imgList[i].addEventListener("click", lightBox);
-	imgList[i].setAttribute("id", "slide" + (i + 1));
+    imgList[i].setAttribute("id", "slide" + (i + 1));
+
+    lightBoxThumb.appendChild(imgList[i].cloneNode(true));
 }
 
 function lightBox(event) {
+    const imgSelecionado = event.target;
+    
+    const imgSelecionadoLightbox = imgSelecionado.cloneNode(true);
+    lightBoxPrincipal.appendChild(imgSelecionadoLightbox);
     document.querySelector("#lightBoxModal").style.display = "block";
+   
+
+    
 
 }
 
-Xclick.addEventListener("click", CloselightBox);
 
-function CloselightBox(event){
-    setTimeout($("#lightBoxModal").fadeOut("slow"), 2000);
+
+function CloselightBox(){    
+    setTimeout($("#lightBoxModal").fadeOut("slow"), 2000);    
 }
