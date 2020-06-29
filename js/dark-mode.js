4e550f601f4f0cdabe96e61daab7512046087ac9
Pedro M <phrm000@gmail.com>
2020-06-29 10:57:11 -0300

    Dark mode em fase de estruturação e links feitos

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,60 +1,67 @@
-const chaveNoturno = document.querySelector('input[name=theme]');
-const imgAboutme = document.querySelector('.about-me-img');
-
-const emailIcon = document.querySelector('#emailIcon');
-const phoneIcon = document.querySelector('#phoneIcon');
-const linkedinIcon = document.querySelector('#linkedinIcon');
-const gitIcon = document.querySelector('#gitIcon');
-
-const cvPT = document.querySelector('#C1');
-const cgPT = document.querySelector('#C2');
-const cvEN = document.querySelector('#C3');
-const cgEN = document.querySelector('#C4');
 
 
-const MvtOnline = document.querySelector('#mvtOnlinepage');
+const chaveNoturno = document.querySelector("input[name=theme]");
+const imgAboutme = document.querySelector(".about-me-img");
+const darkModeTransition = document.querySelector("html");
+console.log(darkModeTransition);
+const emailIcon = document.querySelector("#emailIcon");
+const phoneIcon = document.querySelector("#phoneIcon");
+const linkedinIcon = document.querySelector("#linkedinIcon");
+const gitIcon = document.querySelector("#gitIcon");
+
+const cvPT = document.querySelector("#C1");
+const cgPT = document.querySelector("#C2");
+const cvEN = document.querySelector("#C3");
+const cgEN = document.querySelector("#C4");
+
+const MvtOnline = document.querySelector("#mvtOnlinepage");
 
 
 
-chaveNoturno.addEventListener('change', function() {
-    if(this.checked) {
-        trans()
-        localStorage
-        document.documentElement.setAttribute('data-theme', 'dark');
-        imgAboutme.setAttribute("src","img/aboutMeDark.svg");
-        emailIcon.setAttribute("src","img/emailIconDark.svg");
-        phoneIcon.setAttribute("src","img/phoneIconDark.svg");
-        linkedinIcon.setAttribute("src","img/linkedinIconDark.svg");
-        gitIcon.setAttribute("src","img/gitIconDark.svg");    
-        cvPT.setAttribute("src","img/cvPTDark.svg");
-        cgPT.setAttribute("src","img/cgPTDark.svg");
-        cvEN.setAttribute("src","img/cvENDark2.svg");
-        cgEN.setAttribute("src","img/grENDark.svg");
-        
-        
-        
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light');
-        imgAboutme.setAttribute("src","img/aboutMe.svg");
-        emailIcon.setAttribute("src","img/emailIcon.svg");
-        phoneIcon.setAttribute("src","img/phoneIcon.svg");
-        linkedinIcon.setAttribute("src","img/linkedinIcon.svg");
-        gitIcon.setAttribute("src","img/gitIcon.svg");
-        cvPT.setAttribute("src","img/cvPT.svg");
-        cgPT.setAttribute("src","img/cgPT.svg");
-        cvEN.setAttribute("src","img/cvEN.svg");
-        cgEN.setAttribute("src","img/grEN.svg");
-        
-        
+document.addEventListener("DOMContentLoaded", checkMode);
+chaveNoturno.addEventListener("change", changeMode);
+addEventListener("load",function(){
+    darkModeTransition.style.transition=" background-color 750ms, color 750ms, box-shadow 750ms";
+});
 
-        
+
+function changeMode() {
+    
+    let mode = "",
+        modeS = "light";
+    if (chaveNoturno.checked) {
+        mode = "dark";
+        modeS = mode;
     }
-})
 
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition')
-    }, 1000)
+    localStorage.setItem("mode", chaveNoturno.checked);
+    document.documentElement.setAttribute("data-theme", modeS);
+    if (document.documentURI.split("/")[4] == undefined) {
+
+        imgAboutme.setAttribute("src", `img/aboutMe${mode}.svg`);
+        emailIcon.setAttribute("src", `img/emailIcon${mode}.svg`);
+        phoneIcon.setAttribute("src", `img/phoneIcon${mode}.svg`);
+        linkedinIcon.setAttribute("src",`img/linkedinIcon${mode}.svg`);
+        gitIcon.setAttribute("src",`img/gitIcon${mode}.svg`);
+        cvPT.setAttribute("src", `img/cvPT${mode}.svg`);
+        cgPT.setAttribute("src",`img/cgPT${mode}.svg`);
+        cgEN.setAttribute("src", `img/grEN${mode}.svg`);
+        cvEN.setAttribute("src",`img/cvENDark2.svg`);
+    }
+    
 }
+
+function checkMode() {
+    darkModeTransition.style.display="block"
+    if (localStorage.getItem("mode") == "true") {
+        darkModeTransition.classList.remove(".htmlTransitionDark");
+        
+        chaveNoturno.checked = true;
+        changeMode();
+        
+
+    
+    }
+}
+
+
